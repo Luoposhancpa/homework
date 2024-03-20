9abef3177426270229cad4f1843e68a7260a5793
18356 <1835615816@qq.com>
2024-03-20 22:04:07 +0800

    第四次作业

---
+++ assignment4/Clock/Clock/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Timers;
+using System.Threading.Tasks;
+
+namespace Clock
+{
+     public delegate void clockTickandler();
+    public delegate void clockAlarmHandler();
+    class Clock
+    {
+        public clockTickandler clockTick;
+        public clockTickandler clockAlarm;
+        public void OnTick()
+        {
+            clockTick();
+        }
+        public void OnAlarm()
+        {
+            clockAlarm();
+        }
+        public void start(int time)//闹铃time秒后响铃
+        {
+            for(int i = 0; i < time; i++)
+            {
+                System.Threading.Thread.Sleep(1000);
+                OnTick();
+            }
+            OnAlarm();
+        }
+    }
+    class Boy
+    {
+        internal void Action(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine("Jump");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Clock clock = new Clock();
+            clock.clockTick += () =>
+            {
+                Console.WriteLine("滴答");
+            };
+            clock.clockAlarm += () =>
+            {
+                Console.WriteLine("闹铃响了，响了");
+            };
+            clock.start(9);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ assignment4/ForEach/ForEach/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace ForEach
+
+{
+    public class GenericArray<T>
+    {
+        private T[] array;
+        private int size;
+
+        public GenericArray(int size)
+        {
+            array = new T[size];
+        }
+        public int length() {
+            return array.Length;
+        }
+
+        public T getItem(int index)
+        {
+            return array[index];
+        }
+
+        public void setItem(int index, T value)
+        {
+            array[index] = value;
+        }
+        
+
+        public void ForEach(Action<T> f)
+        {
+            if (f == null)
+                throw new ArgumentNullException();
+            
+            for(int i=0; i<array.Length; i++)
+            {
+                f(array[i]);
+            } 
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericArray<int> array = new GenericArray<int>(5);
+            for (int i = 0; i < array.length();i++)
+            {
+                array.setItem(i, i);
+            }
+            array.ForEach(x =>
+            {
+                Console.WriteLine(x);
+            });
+            int max = int.MinValue;
+            array.ForEach(x =>
+            {
+                if(max<x)
+                    max = x;
+            });
+            Console.WriteLine("最大值：" + max);
+            int min = int.MaxValue;
+            array.ForEach(x =>
+            {
+                if (min> x)
+                    min = x;
+            });
+            Console.WriteLine("最小值：" + min);
+            int sum = 0;
+            array.ForEach(x =>
+            {
+                sum += x;
+            });
+            Console.WriteLine("和：" + sum);
+            Console.ReadKey();
+        }
+    }
+}
